0d9dd7f18d8702b528c3eaff8119a17eb19ccac1
unusualcatcher <152490869+unusualcatcher@users.noreply.github.com>
2023-11-30 16:06:07 +0530

    Update settings.py
    new commit

--- anime_chat_app/settings.py
+++ anime_chat_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-00ejr3$lw40)nu8ojp70w29ju(c+jgol94_m3-otie-csmt^@t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,20 +83,10 @@ WSGI_APPLICATION = 'anime_chat_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'root',
-        'PASSWORD': 'Thisisrootpassword12345',
-        'HOST': 'localhost',   # Use 'localhost' if running locally
-        'PORT': '3306',   # Default MySQL port is usually 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
